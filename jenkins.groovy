44be7f7b89a049ebdef5d6fe7507287dbfff4e13
ASHISH  WAYACHAL <154004776+Ashishwayachal12@users.noreply.github.com>
2026-03-03 09:18:22 +0530

    Add Jenkins pipeline for Maven project build and deploy

---
+++ jenkins.groovy
@@ -0,0 +1,37 @@
+	pipeline {
+	    agent { label "zero" }
+	
+	    stages {
+	
+	        stage('Hello') {
+	            steps {
+	                echo 'Hello Buddy!'
+	            }
+	        }
+	
+	        stage('Git clone') {
+	            steps {
+	                git url: "https://github.com/Ashishwayachal12/Maven_Project2.git", branch: "master"
+	                echo "Code cloning successful"
+	            }
+	        }
+	
+	        stage('Maven Build') {
+	            steps {
+	                sh 'mvn clean install'
+	            }
+	        }
+	
+	        stage('Run Jar') {
+	            steps {
+	                sh 'java -jar target/MavenProject-v4.1.jar'
+	            }
+	        }
+	
+	        stage('Deploy') {
+	            steps {
+	                echo 'Deployed the code...........'
+	            }
+	        }
+	    }
+	}
